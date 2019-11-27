6a96a958648cbca7b8a0c873b2d15aef0833f1f8
zhaiyuanhang <774855001@qq.com>
2019-11-27 14:33:42 +0800

    updata

---
+++ dist/service-worker.js
@@ -0,0 +1,23 @@
+importScripts("/precache-manifest.484295925876e48bd167997d4d9c6c46.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+self.addEventListener('message', (e) => {
+    if (!e.data) {
+      return;
+    }
+  
+    switch (e.data) {
+      case 'skipWaiting':
+        self.skipWaiting();
+        break;
+      default:
+        // NOOP
+        break;
+    }
+  });
+  
+  workbox.clientsClaim();
+  
+  // The precaching code provided by Workbox.
+  self.__precacheManifest = [].concat(self.__precacheManifest || []);
+  workbox.precaching.suppressWarnings();
+  workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
